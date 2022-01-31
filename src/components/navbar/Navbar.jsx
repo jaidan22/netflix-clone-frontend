8a2d9d3f143b96d04a69a405a03b7298a6815a39
jaidan22 <jaidansleeba22@gmail.com>
2022-01-31 15:55:40 +0530

    updated image source

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import React, { useState } from "react";
 import "./navbar.scss";
+import profile from "../../images/profile.jpg";
+import logo from "../../images/icon.png";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -16,10 +18,7 @@ const Navbar = () => {
     <div className={isScrolled ? "navbar scrolled" : "navbar"}>
       <div className="container">
         <div className="left">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/799px-Netflix_2015_logo.svg.png"
-            alt="netflix logo"
-          />
+          <img src={logo} alt="netflix logo" />
           <span>Home</span>
           <span>TV Shows</span>
           <span>Movies</span>
@@ -30,10 +29,7 @@ const Navbar = () => {
           <Search className="icon" />
           <span>Kids</span>
           <Notifications className="icon" />
-          <img
-            src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
-            alt="user"
-          />
+          <img src={profile} alt="user" />
           <div className="profile">
             <ArrowDropDown className="icon" />
             <div className="options">
